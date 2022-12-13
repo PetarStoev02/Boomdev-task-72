2e9ace0048aba16f5cfb20e3278cad1103151624
Петър Стоев <petarstoev0505@gmail.com>
2022-12-13 16:53:23 +0200

    v1

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import "./App.css";
+import Tags from "./Tags";
+
+console.log(Tags)
+
 
 function App() {
   return (
-    <div className="App">
-      <section class="hero">
-        <div class="hero-body">
-          <p class="title">A React Task</p>
-          <p class="subtitle">by Boom.dev</p>
-        </div>
-      </section>
-      <div class="container is-fullhd">
-        <div class="notification">
-          Edit the <code>./src</code> folder to add components.
-        </div>
-      </div>
-    </div>
+    // <div className="App">
+    //   <section class="hero">
+    //     <div class="hero-body">
+    //       <p class="title">A React Task</p>
+    //       <p class="subtitle">by Boom.dev</p>
+    //     </div>
+    //   </section>
+    //   <div class="container is-fullhd">
+    //     <div class="notification">
+    //       Edit the <code>./src</code> folder to add components.
+    //     </div>
+    //   </div>
+    // </div>
+
+    <Tags />
   );
+
+  
 }
 
 export default App;
---
+++ src/Tags.js
@@ -0,0 +1,13 @@
+function Tags() {
+  let tags = ["boomdotdev", "task", "tags", "react"];
+  return (
+    <div class="tags">
+      <div class="tag"># {tags[0]}</div>
+      <div class="tag"># {tags[1]}</div>
+      <div class="tag"># {tags[2]}</div>
+      <div class="tag"># {tags[3]}</div>
+    </div>
+  );
+}
+
+export default Tags;
